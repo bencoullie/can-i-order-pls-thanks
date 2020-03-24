6676f9747f28d15de13122e0a6773b5e851f648c
Benjamin Coullie <ben.coullie@vendhq.com>
2020-03-24 20:45:20 +1300

    add interval

--- puppeteer-scripty-boi.js
+++ puppeteer-scripty-boi.js
@@ -4,16 +4,36 @@ const { wait } = require("./wait")
 
 const setSuburbPage = "https://shop.countdown.co.nz/shop/showaddresses"
 const getDisappointedPage = "https://shop.countdown.co.nz/shop/deliverydetails"
-const slotNotAvailableWhatASurprise = "(No available time slots)"
-const suburb = process.argv.slice(2)[0]
+const suburb = process.argv[2]
+const intervalInMinutes = process.argv[3] * 60000
 const suburbInput = "input[name=PostcodeSuburb]"
 const suburbSubmitButton = "input[type=submit]"
 
+const getDisappointedBra = async page => {
+  await page.screenshot({ path: "latest-screenshot.png", fullPage: true })
+
+  const slotsAvailable = await page.evaluate(() => {
+    let openSlots = [...document.querySelectorAll(".open-delivery-window")]
+
+    return openSlots.some(slot => getComputedStyle(slot, null).display === "block")
+  })
+
+  notifier.notify({
+    title: "Countdown Check",
+    message: slotsAvailable ? "✅ Get in there quick! ✅" : "❌ Still no slots available! ❌"
+  })
+}
+
+const reloadAndRecheck = async page => {
+  await page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] })
+  await getDisappointedBra(page)
+}
+
 const checkIfShitsFinallyReady = async () => {
-  const browser = await puppeteer.launch({ headless: false, defaultViewport: { width: 2560, height: 1440 } })
+  const browser = await puppeteer.launch({ defaultViewport: { width: 2560, height: 1440 } })
   const page = await browser.newPage()
 
-  await page.goto(setSuburbPage, { waitUntil: "networkidle2" })
+  await page.goto(setSuburbPage, { waitUntil: "networkidle0" })
   await page.waitFor(suburbInput)
 
   await page.evaluate(suburbInput => {
@@ -27,22 +47,11 @@ const checkIfShitsFinallyReady = async () => {
   await page.click(suburbSubmitButton)
   await wait(2)
 
-  await page.goto(getDisappointedPage, { waitUntil: "networkidle2" })
+  await page.goto(getDisappointedPage, { waitUntil: "networkidle0" })
 
-  await page.screenshot({ path: "latest-screenshot.png", fullPage: true })
+  await getDisappointedBra(page)
 
-  const slotsAvailable = await page.evaluate(() => {
-    let openSlots = [...document.querySelectorAll(".open-delivery-window")]
-
-    return openSlots.some(slot => getComputedStyle(slot, null).display === "block")
-  })
-
-  notifier.notify({
-    title: "Countdown Check",
-    message: slotsAvailable ? "✅ Get in there quick! ✅" : "❌ Still no slots available! ❌"
-  })
-
-  await browser.close()
+  setInterval(() => reloadAndRecheck(page), intervalInMinutes)
 }
 
 checkIfShitsFinallyReady()
